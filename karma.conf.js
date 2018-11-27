c8ad4ef9677292b84d018e5522a46bc44e2ef7d8
l5x <189198+l5x@users.noreply.github.com>
2018-11-28 02:57:45 +0400

    chore: saucelabs karma plugin

--- karma.conf.js
+++ karma.conf.js
@@ -12,14 +12,13 @@ module.exports = config => {
     client: {
       captureConsole: false
     },
-    plugins: [
-      'karma-chrome-launcher',
-      'karma-mocha',
-      'karma-sauce-launcher',
-      'karma-typescript'
-    ],
+    plugins: ['karma-chrome-launcher', 'karma-mocha', 'karma-typescript'],
     frameworks: ['mocha', 'karma-typescript'],
-    files: ['node_modules/@babel/polyfill/dist/polyfill.js', 'src/**/*.ts', 'test/**/*.ts'],
+    files: [
+      'node_modules/@babel/polyfill/dist/polyfill.js',
+      'src/**/*.ts',
+      'test/**/*.ts'
+    ],
     preprocessors: {
       'src/**/*.ts': 'karma-typescript',
       'test/**/*.ts': 'karma-typescript'
@@ -45,45 +44,4 @@ module.exports = config => {
       tsconfig: './tsconfig.json'
     }
   })
-
-  if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
-    const customLaunchers = {
-      sl_firefox: {
-        base: 'SauceLabs',
-        browserName: 'Firefox'
-      },
-      sl_chrome: {
-        base: 'SauceLabs',
-        browserName: 'Chrome',
-        version: 'beta'
-      },
-      sl_safari: {
-        base: 'SauceLabs',
-        browserName: 'Safari'
-      },
-      sl_edge: {
-        base: 'SauceLabs',
-        browserName: 'MicrosoftEdge'
-      }
-    }
-
-    config.set({
-      retryLimit: 3,
-      concurrency: 2,
-      captureTimeout: 85 * 1000,
-      browserNoActivityTimeout: 120 * 1000,
-      browserDisconnectTimeout: 15 * 1000,
-      browserDisconnectTolerance: 3,
-      sauceLabs: {
-        testName: `${name} karma test`,
-        tunnelIdentifier: process.env.TRAVIS
-          ? process.env.TRAVIS_JOB_NUMBER
-          : camelCase(name)
-      },
-      customLaunchers: customLaunchers,
-      browsers: Object.keys(customLaunchers),
-      reporters: ['dots', 'karma-typescript', 'saucelabs'],
-      singleRun: true
-    })
-  }
 }
