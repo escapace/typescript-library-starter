e8e234fa5b7c653110ab12529f816304cc082d7d
Mark Martirosian <hello@markmartirosian.com>
2021-05-31 10:35:44 +0400

    SAVEPOINT

--- husky.config.js
+++ husky.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   hooks: {
     'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
-    'pre-commit': 'lint-staged && npm run typecheck && npm run test'
+    'pre-commit': 'ls-lint && lint-staged && npm run typecheck && npm run test'
   }
 }
